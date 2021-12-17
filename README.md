c504d5cb9a5728b0e2d02e0e04cc54b537d3fcf7
Josue Barros <joshbarrosweb@gmail.com>
2021-12-17 16:19:26 -0300

    chore: first commit

--- README.md
+++ README.md
@@ -1,70 +1,24 @@
-# Getting Started with Create React App
+# WeDigi TodoChallenge
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Projeto desafio: Todo CRUD com possibilidade de reordenação de elementos.
 
-## Available Scripts
+## Como foi desenvolvido o projeto?
 
-In the project directory, you can run:
+As estruturas de dados utilizadas foram: Objeto e Array.
 
-### `yarn start`
+Para desenvolver a todo experimental foi utilizado um simples objeto "todo", contendo um array de "items".
+Ao cadastrar o novo item, é gerado dinamicamente um ID com uuid para evitar duplicidade nas chaves dos elementos,
+e é inserido o nome desejado.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Ao clicar e arrastar, é possivel reordenar os todos na lista graças a ajuda da biblioteca "react-beautiful-dnd" onde temos um context "DragAndDrop" que permite reordenar os items livremente.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Ao clicar em editar é exibido um prompt simples, onde caso seja digitado o novo nome, ele é trocado.
 
-### `yarn test`
+Ao clicar em deletar, o todo é excluido da lista (caso haja confirmação).
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Caso o usuario tente soltar o elemento fora da area de contexto, o elemento é resetado, visto que ele não é removido
+ao ser clicado, somente quando ele é soltado, onde sua localização antiga é removida da memoria e a nova é registrada.
 
-### `yarn build`
+### Como rodar o projeto
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### `npm install && npm run start`
--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import './styles/App.css';
+import _ from 'lodash';
+import { v4 } from 'uuid';
 
 function App() {
+  const [state, setState] = useState({
+    todo: {
+      items: [],
+    },
+  });
+
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      return;
+    }
+
+    const copiedItem = { ...state[source.droppableId].items[source.index] };
+    setState((prev) => {
+      prev = { ...prev };
+      prev[source.droppableId].items.splice(source.index, 1);
+
+      prev[destination.droppableId].items.splice(
+        destination.index,
+        0,
+        copiedItem
+      );
+
+      return prev;
+    });
+  };
+
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    setState((prev) => {
+      return {
+        ...prev,
+        todo: {
+          items: [
+            {
+              id: v4(),
+              name: text,
+            },
+            ...prev.todo.items,
+          ],
+        },
+      };
+    });
+  };
+
+  const deleteTodo = (id) => {
+    let shouldDelete = confirm(`Deseja excluir o item?`);
+
+    if (shouldDelete == true) {
+      const todoRemove = state.todo.items.filter((todo) => {
+        let toBeDeleted = todo.id !== id;
+
+        return toBeDeleted;
+      });
+
+      setState({
+        todo: {
+          items: todoRemove,
+        },
+      });
+    }
+  };
+
+  const editTodo = (id) => {
+    let todoEdit = state.todo.items.find((item) => item.id == id);
+
+    let newName = prompt(`Digite o novo nome para o elemento \n \n${id}: `);
+
+    console.log(todoEdit);
+
+    if (newName) {
+      const todos = [...state.todo.items];
+      const indexOfTodo = state.todo.items.findIndex((todo) => todo.id === id);
+      todos[indexOfTodo].name = newName;
+      setState({
+        todo: {
+          items: todos,
+        },
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="add-todo-form-wrapper">
+        <h1>WeDigi - Todo Challenge</h1>
+        <input
+          className="add-todo-form"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addTodo}>Adicionar</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return (
+            <div key={key} className={'column'}>
+              <Droppable droppableId={key}>
+                {(provided) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={'droppable-col'}
+                    >
+                      {data.items.map((el, index) => {
+                        return (
+                          <Draggable
+                            key={el.id}
+                            index={index}
+                            draggableId={el.id}
+                          >
+                            {(provided) => {
+                              return (
+                                <div
+                                  className={'item'}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                  <div className={'options'}>
+                                    <button
+                                      onClick={() => editTodo(el.id)}
+                                      className="edit-todo"
+                                    >
+                                      E
+                                    </button>
+                                    <button
+                                      onClick={() => deleteTodo(el.id)}
+                                      className="delete-todo"
+                                    >
+                                      X
+                                    </button>
+                                  </div>
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
